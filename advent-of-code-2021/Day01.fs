8f4f2ce6af14a2061cbce2839f13c88f10d42df9
andi0b <andix25@gmail.com>
2021-12-01 19:45:29 +0100

    day1

---
+++ advent-of-code-2021/Day01.fs
@@ -0,0 +1,41 @@
+﻿module advent_of_code_2021.Day01
+
+open System.IO
+
+
+let parse (strs: string seq) = strs |> Seq.map int |> Seq.toList
+
+let zipshift left right =
+    List.zip (left |> List.take (left.Length - 1)) (right |> List.skip 1)
+
+let part1 (numbers: int list) =
+    zipshift numbers numbers
+    |> Seq.filter (fun (a, b) ->  b > a)
+    |> Seq.length
+
+let part2 (numbers: int list) =
+    zipshift (numbers |> List.take (numbers.Length - 1)) (zipshift numbers numbers)
+    |> List.map (fun (a, (b, c)) -> a + b + c)
+    |> part1
+
+let test =
+    let parsed =
+        [ "199"
+          "200"
+          "208"
+          "210"
+          "200"
+          "207"
+          "240"
+          "269"
+          "260"
+          "263" ]
+        |> parse
+
+    assert (parsed |> part1 = 7)
+    assert (parsed |> part2 = 5)
+
+let result =
+    test
+    let parsed = File.ReadAllLines("Day01.txt") |> parse
+    $"{part1 parsed}/{part2 parsed}"
---
+++ advent-of-code-2021/Program.fs
@@ -0,0 +1,10 @@
+
+
+// For more information see https://aka.ms/fsharp-console-apps
+
+open advent_of_code_2021
+
+printfn "Hello from F#"
+
+
+printf $"Day1 {Day01.result}"
